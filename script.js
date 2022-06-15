837db61092454a69f82e0e2c4a82ee4bf11d96d1
Tim Polo <timothyjpolo@gmail.com>
2022-06-16 06:21:47 +1000

    Portfolio Updated to have more projects

--- script.js
+++ script.js
@@ -13,11 +13,14 @@ var index = 0;
 var currentImage;
 
 var images = {
-    0: { file: "./assets/images/lanesBridge.jpg", project: 'Lanes Bridge', description: '60m x 10m x 10m Concrete Bridge', link: "https://www.nbnnews.com.au/2019/08/20/ribbon-cutting-marks-official-opening-of-new-lanes-bridge/" },
-    1: { file: "./assets/images/WeatherDashboard.JPG", project: 'Weather Dashboard', description: 'Enter a city for weather data', link: 'https://timp1990.github.io/Weather-Dashboard/', gitHub: "https://github.com/timp1990/Weather-Dashboard" },
-    2: { file: "./assets/images/WorkdayScheduler.JPG", project: 'Workday Scheduler', description: 'A daily planner for a standard workday', link: 'https://timp1990.github.io/Work-Day-Scheduler/', gitHub: "https://github.com/timp1990/Work-Day-Scheduler" },
-    3: { file: "./assets/images/Fischers Aerial.JPG", project: 'Fischers Bridge', description: '35m x 5m x 10m Concrete Bridge', link: "https://www.nbnnews.com.au/2020/06/17/largest-mobile-crane-used-in-bridge-build-at-tamban/" },
-    4: { file: "./assets/images/3b Nyora Close Photo.JPG", project: 'Lanes Bridge', description: 'Coffs Harbour Residence', link: "https://www.google.com/maps/place/3+Nyora+Cl,+Coffs+Harbour+NSW+2450/@-30.3052227,153.1065517,18.87z/data=!4m5!3m4!1s0x6b9c0e0af710ed7b:0x79cf4433a5d46d1b!8m2!3d-30.3052845!4d153.1068923" },
+    0: { file: "./assets/images/techblog.JPG", project: 'Tech Blog', description: 'Blog Website for Tech People', link: "https://dry-sea-52050.herokuapp.com/", gitHub: "https://github.com/timp1990/Tech-Blog" },
+    1: { file: "./assets/images/projecthub.JPG", project: 'Project Hub - 1 of a team of 4', description: 'Part of a team that made a project management website', link: "https://projecthub-crm.herokuapp.com/login", gitHub: "https://github.com/wilgru/projecthub" },
+    2: { file: "./assets/images/lanesBridge.jpg", project: 'Lanes Bridge', description: '60m x 10m x 10m Concrete Bridge', link: "https://www.nbnnews.com.au/2019/08/20/ribbon-cutting-marks-official-opening-of-new-lanes-bridge/" },
+    3: { file: "./assets/images/teamprofile.JPG", project: 'Team Profile Generator', description: 'Use Inquirer and Node to generate a website for a Team Profile', link: "https://www.youtube.com/watch?v=TQAOtdu7KJ0", gitHub: "https://github.com/timp1990/Team-Profile-Generator" },
+    4: { file: "./assets/images/WeatherDashboard.JPG", project: 'Weather Dashboard', description: 'Enter a city for weather data', link: 'https://timp1990.github.io/Weather-Dashboard/', gitHub: "https://github.com/timp1990/Weather-Dashboard" },
+    5: { file: "./assets/images/WorkdayScheduler.JPG", project: 'Workday Scheduler', description: 'A daily planner for a standard workday', link: 'https://timp1990.github.io/Work-Day-Scheduler/', gitHub: "https://github.com/timp1990/Work-Day-Scheduler" },
+    6: { file: "./assets/images/Fischers Aerial.JPG", project: 'Fischers Bridge', description: '35m x 5m x 10m Concrete Bridge', link: "https://www.nbnnews.com.au/2020/06/17/largest-mobile-crane-used-in-bridge-build-at-tamban/" },
+    7: { file: "./assets/images/3b Nyora Close Photo.JPG", project: 'Residence', description: 'Coffs Harbour Residence', link: "https://www.google.com/maps/place/3+Nyora+Cl,+Coffs+Harbour+NSW+2450/@-30.3052227,153.1065517,18.87z/data=!4m5!3m4!1s0x6b9c0e0af710ed7b:0x79cf4433a5d46d1b!8m2!3d-30.3052845!4d153.1068923" },
 };
 
 carouselImg.setAttribute('src', images[0]);
